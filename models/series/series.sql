6075d79d00e0470473f75d3649e46b4d3faa5f55
Marko Todorcevic <todorcevic.marko.uni@gmail.com>
2021-10-27 12:28:48 +0200

    Create a new Series model

---
+++ models/series/series.sql
@@ -0,0 +1,37 @@
+select 
+	cast(replace(show_id, 's', '') as integer) as "id", 
+
+	title, 
+
+	case 
+		when director like '%,%' then left(director, position(',' in director) - 1)
+		else director
+	end, 
+
+	"cast", 
+
+	case 
+		when country like '%,%' then left(country, position(',' in country) - 1)
+		else country
+	end, 
+
+	to_date(date_added, 'Month DD, YYYY'), 
+
+	cast(release_year as integer), 
+
+	rating, 
+
+	case 
+		when duration like '%Season%' then cast(left(duration, position('S' in duration) - 1) as integer)
+	end as number_of_seasons, 
+
+	case 
+		when listed_in like '%,%' then left(listed_in, position(',' in listed_in) - 1) 
+		else listed_in
+	end as genre, 
+
+	"description"
+
+from {{ source('raw_data', 'titles') }} 
+
+where "type" = 'TV Show'
