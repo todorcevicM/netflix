206c6ec3eaa461a307839d30b7921a0ebab7cace
Marko Todorcevic <todorcevic.marko.uni@gmail.com>
2021-11-01 16:45:44 +0100

    Delete t.sql, as well as replace case with statements with coalesce function

--- models/series/actors.sql
+++ models/series/actors.sql
@@ -4,8 +4,7 @@ with ranked_genre_frequency as (
 		select 
 			actor_name, 
 			genre as series_genre, 
-			count(*) as series_frequency, 
-			count(series.id) as series_count 
+			count(*) as series_frequency
 
 		from {{ ref('title_actor') }} 
 
@@ -24,9 +23,7 @@ with ranked_genre_frequency as (
 		), 
 
 		series_frequency, 
-		null as movie_frequency, 
-		series_count, 
-		null as movie_count 
+		null as movie_frequency
 		
 	from series_genre_freq 
 	
@@ -37,8 +34,7 @@ with ranked_genre_frequency as (
 			select 
 				actor_name, 
 				genre as movie_genre, 
-				count(*) as movie_frequency, 
-				count(movies.id) as movie_count 
+				count(*) as movie_frequency
 
 			from {{ ref('title_actor') }} 
 			
@@ -58,9 +54,7 @@ with ranked_genre_frequency as (
 			), 
 				
 			null as series_frequency, 
-			movie_frequency, 
-			null as series_count, 
-			movie_count 
+			movie_frequency
 			
 		from movie_genre_frequency 
 	)
@@ -68,35 +62,20 @@ with ranked_genre_frequency as (
 ) 
 select
 	actor_name as "name", 
+	coalesce(max(series_frequency), 0)	as series_frequency,  
+	coalesce(max(movie_frequency), 0) as movie_frequency, 
 
 	case 
-		when max(series_count) is not null then max(series_count)
-		else 0
-	end	as series_count,  
-
-	case 
-		when max(movie_count) is not null then max(movie_count) 
-		else 0
-	end as movie_count, 
-
-	case 
-		when max(series_genre) is not null and max(movie_genre) is not null and max(series_genre) >= max(movie_genre) then max(series_genre) 
-		when max(series_genre) is not null and max(movie_genre) is not null and max(movie_genre) > max(series_genre) then max(movie_genre) 
-		when max(series_genre) is not null and max(movie_genre) is null then max(series_genre) 
-		when max(movie_genre) is not null then max(movie_genre) 
+		when coalesce(max(series_frequency), 0) >= coalesce(max(movie_frequency), 0) then max(series_genre) 
+		else max(movie_genre) 
 	end as most_common_genre, 
 
-	case 
-		when max(series_frequency) is not null and max(movie_frequency) is not null and max(series_frequency) >= max(movie_frequency) then max(series_frequency) 
-		when max(series_frequency) is not null and max(movie_frequency) is not null and max(movie_frequency) > max(series_frequency) then max(movie_frequency)
-		when max(series_frequency) is not null and max(movie_frequency) is null then max(series_frequency)
-		else max(movie_frequency)
-	end as most_common_genre_titles_count
+	coalesce(greatest(max(series_frequency), max(movie_frequency)), 0) as most_common_genre_titles_count 	
 
 from ranked_genre_frequency 
 
 where "row_number" = 1 
 
-group by actor_name 
+group by actor_name
 
 order by most_common_genre_titles_count desc 
