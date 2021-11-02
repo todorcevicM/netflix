6fb70191ef6844a7263a59c77ecd718795602112
Marko Todorcevic <todorcevic.marko.uni@gmail.com>
2021-11-02 11:07:08 +0100

    Add rows about the number of appearences in shows

--- models/series/actors.sql
+++ models/series/actors.sql
@@ -1,5 +1,5 @@
 with ranked_genre_frequency as (
-
+	
 	with titles_genre_frequency as (
 		select 
 			actor_name, 
@@ -42,9 +42,47 @@ with ranked_genre_frequency as (
 ) 
 select
 	actor_name as "name", 
+	movie_count, 
+	series_count, 
 	genre as "most_common_genre", 
 	frequency as "most_common_genre_count" 
 
 from ranked_genre_frequency 
 
-where "row_number" = 1 
+join (
+	select 
+		coalesce(movies_counts.actor_name, series_counts.actor_name) as "name", 
+		coalesce(movie_count, 0) as movie_count, 
+		coalesce(series_count, 0) as series_count
+
+	from (
+		select 
+			actor_name, 
+			count(*) as movie_count 
+			
+		from {{ ref('title_actor') }} 
+		
+		where movie_id is not null
+
+		group by actor_name
+	) movies_counts
+
+	full outer join (
+		select 
+			actor_name, 
+			count(*) as series_count 
+			
+		from {{ ref('title_actor') }} 
+		
+		where series_id is not null
+
+		group by actor_name
+	) series_counts
+	on movies_counts.actor_name = series_counts.actor_name 
+	
+	order by movie_count desc
+) titles_count 
+
+on titles_count.name = ranked_genre_frequency.actor_name
+
+where "row_number" = 1
